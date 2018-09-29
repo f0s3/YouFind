cae81a7d57ebfdd8b97c8e8cfce77e3402b415f0
vladyslavnes <easterenko@gmail.com>
2018-09-29 12:02:47 +0300

    Basic structure

---
+++ frontend/src/components/DevicesBox/DevicesBox.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {  } from '../DeviceCard/index';
+
+export default class DevicesBox extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
